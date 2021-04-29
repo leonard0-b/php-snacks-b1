b35e1a0c0ff2b3785b8040c4bd9813fdde803755
Leonardo <leonardo.bongianni@gmail.com>
2021-04-29 15:01:24 +0200

    PhpSnack1

---
+++ 1/index.php
@@ -0,0 +1,34 @@
+<?php 
+  $campionato = [
+    [
+      "squadracasa" => "Atletico Perniente",
+      "squadraospite" => "Sestese",
+      "punticasa" => "78",
+      "puntiospite" => "120",
+    ],
+    [
+      "squadracasa" => "Fiorentina",
+      "squadraospite" => "Juventus",
+      "punticasa" => "100",
+      "puntiospite" => "0",
+    ],
+    [
+      "squadracasa" => "Ciaoino",
+      "squadraospite" => "Ciaoetto",
+      "punticasa" => "78",
+      "puntiospite" => "120",
+    ],
+  ];
+
+  var_dump($campionato);
+  for($i = 0; $i < count($campionato); $i++) {
+    ?> 
+    <div>
+      <?php echo $campionato[$i]["squadracasa"] ?> -
+      <?php echo $campionato[$i]["squadraospite"] ?> |
+      <?php echo $campionato[$i]["punticasa"] ?> -
+      <?php echo $campionato[$i]["puntiospite"] ?>
+    </div>
+    <?php
+  }
+?>
